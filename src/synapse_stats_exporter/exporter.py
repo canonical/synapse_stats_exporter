dc2e0712b8165cad05d6eb1a77672780773ed5f4
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2024-03-07 15:55:22 -0300

    Use user and password instead of token

--- src/synapse_stats_exporter/exporter.py
+++ src/synapse_stats_exporter/exporter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """Synapse Stats Exporter"""
 
+import json
 import os
 import time
 from prometheus_client import start_http_server, Gauge
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 ENDPOINT_ROOMS = "_synapse/admin/v1/rooms?dir=f&from=0&limit=10"
 ENDPOINT_USERS = "_synapse/admin/v2/users?deactivated=false&dir=f&from=0&guests=true&limit=10"
@@ -15,7 +18,7 @@ class SynapseStatsMetrics:
     Represent Synapse statistics.
     """
 
-    def __init__(self, base_url="http://localhost:8008", admin_token="", polling_interval_seconds=60):
+    def __init__(self, base_url="http://localhost:8008/", admin_token="", polling_interval_seconds=60):
         self.base_url = base_url
         self.admin_token = admin_token
         self.polling_interval_seconds = polling_interval_seconds
@@ -27,7 +30,9 @@ class SynapseStatsMetrics:
     def run_metrics_loop(self):
         """Metrics fetching loop"""
 
+        print("Synapse Stats Exporter started.")
         while True:
+            print("Synapse Stats Exporter will fetch statistics. Next polling in %d seconds.", self.polling_interval_seconds)
             self.fetch()
             time.sleep(self.polling_interval_seconds)
 
@@ -36,22 +41,57 @@ class SynapseStatsMetrics:
         Get metrics from Synapse and refresh Prometheus metrics with
         new values.
         """
+        try:
+            headers = {'Authorization': f'Bearer {self.admin_token}'}
+            response = requests.get(f'{self.base_url}{ENDPOINT_ROOMS}', headers=headers)
+            response.raise_for_status()
+            data = response.json()
+            self.synapse_total_rooms.set(data["total_rooms"])
+            response = requests.get(f'{self.base_url}{ENDPOINT_USERS}', headers=headers)
+            data = response.json()
+            self.synapse_total_users.set(data["total"])
+        except requests.exceptions.RequestException as e:
+            print(f"Fetch request failed. Exception: {e}")
+        except (json.decoder.JSONDecodeError, KeyError) as e:
+            print(f"Fetch read failed. Exception: {e}")
 
-        headers = {'Authorization': f'Bearer {self.admin_token}'}
-        response = requests.get(f'{self.base_url}{ENDPOINT_ROOMS}', headers=headers)
-        data = response.json()
-        self.synapse_total_rooms.set(data["total_rooms"])
-        response = requests.get(f'{self.base_url}{ENDPOINT_USERS}', headers=headers)
-        data = response.json()
-        self.synapse_total_users.set(data["total"])
-
+def login(base_url, user, password):
+    url = base_url + "_synapse/client/r0/login"
+    data = {
+        "type": "m.login.password",
+        "user": user,
+        "password": password
+    }
+    session = requests.Session()
+    retries = Retry(
+        total=3,
+        backoff_factor=3,
+    )
+    session.mount("http://", HTTPAdapter(max_retries=retries))
+    try:
+        response = session.request("POST", url, json=data, timeout=5)
+        response.raise_for_status()
+        response_data = response.json()
+        if "access_token" in response_data:
+            access_token = response_data["access_token"]
+            return access_token
+        else:
+            print("Login failed. Access token not found in response.")
+    except requests.exceptions.RequestException as e:
+        print(f"Login failed. Exception: {e}")
+    finally:
+        session.close()
 
 def main():
     polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "60"))
-    base_url = os.getenv("PROM_SYNAPSE_BASE_URL", "http://localhost:8008")
-    admin_token = os.getenv("PROM_SYNAPSE_ADMIN_TOKEN", "password")
+    base_url = os.getenv("PROM_SYNAPSE_BASE_URL", "http://localhost:8008/")
+    user = os.getenv("PROM_SYNAPSE_USER", "user")
+    password = os.getenv("PROM_SYNAPSE_USER", "password")
     exporter_port = int(os.getenv("EXPORTER_PORT", "9877"))
 
+    admin_token = login(base_url=base_url, user=user, password=password)
+
+    print("Login succeed. Synapse Stats Exporter will be started.")
     synapse_stats_metrics = SynapseStatsMetrics(
         base_url=base_url,
         admin_token=admin_token,
